bf9e498a97a5fadc63ee9045e483fe14006cf2a9
Elijah Lee <develijahlee@gmail.com>
2023-01-23 20:47:25 +0900

    feat: add Formik into Login

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useSession } from 'next-auth/react';
 import { signOut } from 'next-auth/react';
+import Login from '@/components/Login/Login';
 
 export default function Home() {
   const { data: session } = useSession();
@@ -8,9 +9,15 @@ export default function Home() {
 
   return (
     <main>
-      <p>name: {session?.user?.name}</p>
-      <p>email: {session?.user?.email}</p>
-      <button onClick={() => signOut()}>Sign Out</button>
+      {session ? (
+        <>
+          <p>name: {session?.user?.name}</p>
+          <p>email: {session?.user?.email}</p>
+          <button onClick={() => signOut()}>Sign Out</button>
+        </>
+      ) : (
+        <Login />
+      )}
     </main>
   );
 }
--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -2,6 +2,12 @@
 import styles from './Login.module.scss';
 import { signIn } from 'next-auth/react';
 import Link from 'next/link';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+interface LoginFormValues {
+  email: string;
+  password: string
+}
 
 export default function Login() {
   return (
@@ -9,8 +15,43 @@ export default function Login() {
       <div className={styles.formWrapper}>
         <span className={styles.logo}>Live Chat</span>
         <span className={styles.title}>Login</span>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={async (values, { setStatus }) => {
+            // If onSubmit is async, Formik automatically sets isSubmitting to false once it resolves
+            try {
+              console.log('values', values);
+              // const res = await fetch('/api/contact', {
+              //   method: 'POST',
+              //   body: JSON.stringify(values),
+              //   headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+              // });
+              // if (!res.ok) {
+              //   throw new Error(`Error sending message: ${res.status}`);
+              // }
+            } catch (error) {
+              console.error(error);
+              setStatus({ error: 'There was an error.' });
+              throw error;
+            }
+          }}
+        >
+          {({ values, errors, submitCount, status, isSubmitting }) => (
+            <Form>
+              <label htmlFor="email">Email Address</label>
+              <Field id="email" type="email" name="email" />
+              {submitCount > 0 && errors.email && <ErrorMessage name="email" component="div" className={styles.errorMessage} />}
+              <label htmlFor="password">Password</label>
+              <Field id="password" name="password" type="password" />
+              <button type="submit" disabled={isSubmitting || !values.email || !values.password} aria-label="Send email form">
+                Submit
+              </button>
+              {status?.error && <p className={styles.errorStatus}>{status.error}</p>}
+            </Form>
+          )}
+        </Formik>
         <button onClick={() => signIn('google')}>
-          Sign in with Google
+          Login with Google
         </button>
         <p>You don&apos;t have an account? <Link href="/register">Register</Link></p>
       </div>
