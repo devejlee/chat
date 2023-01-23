288d29e9f9f9b600c230609357fa81ae9aaf83cc
Elijah Lee <develijahlee@gmail.com>
2023-01-23 14:37:57 +0900

    feat: connect to firebase firestore

---
+++ utils/firebase.ts
@@ -0,0 +1,16 @@
+
+import * as admin from 'firebase-admin';
+
+const PRIVATE_KEY = process.env.FIREBASE_PRIVATE_KEY as string;
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: PRIVATE_KEY.replace(/\\n/g, '\n')
+    })
+  });
+}
+
+export const database = admin.firestore();
