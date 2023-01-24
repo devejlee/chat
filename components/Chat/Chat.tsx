da2588eddd45fe4c8544cc43c671dedb5310c6cf
Elijah Lee <develijahlee@gmail.com>
2023-01-24 21:05:09 +0900

    feat: add react icons

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -1,6 +1,8 @@
 'use client';
 import styles from './Chat.module.scss';
 import { useSession } from 'next-auth/react';
+import { BsCameraVideoFill, BsPersonPlusFill } from 'react-icons/bs';
+import { FaEllipsisH } from 'react-icons/fa';
 
 export default function Chat() {
   const { data: session } = useSession();
@@ -9,6 +11,9 @@ export default function Chat() {
       <div className={styles.chatInfo}>
         <span>{session?.user?.name}</span>
         <div className={styles.chatIcons}>
+          <BsCameraVideoFill />
+          <BsPersonPlusFill />
+          <FaEllipsisH />
         </div>
       </div>
     </div>
