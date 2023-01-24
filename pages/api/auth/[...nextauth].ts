712ed7c11e7c3771e3605b9c00b7c7d3a1bb039f
Elijah Lee <develijahlee@gmail.com>
2023-01-24 14:10:42 +0900

    feat: add firebase adapter to next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { FirestoreAdapter } from '@next-auth/firebase-adapter';
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -13,6 +14,15 @@ export const authOptions: NextAuthOptions = {
     redirect: async (params: { url: string; baseUrl: string; }) => {
       return '/';
      },
-  }
+  },
+  adapter: FirestoreAdapter({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MESUREMENT_ID
+  }),
 };
 export default NextAuth(authOptions);
--- pages/api/firebaseAuth.ts
+++ pages/api/firebaseAuth.ts
@@ -1,21 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { firebaseAuth } from '@/utils/firebase';
+import { database, firebaseAuth } from '@/utils/firebase';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const data = req.body;
     try {
-      await firebaseAuth.createUser({ 
+      const newUser = {
         email: data.email,
         password: data.password,
         displayName: data.name
-      });
-      return res.status(201).json({
-        message: 'User created successfully',
-      });
+      };
+      const userRef = database.collection('users');
+      const userSnapshot = await userRef.where('email', '==', data.email).get();
+      if (userSnapshot.empty) {
+        await firebaseAuth.createUser(newUser);
+        await userRef.add(newUser);
+        return res.status(201).json({
+          message: 'User created successfully',
+        });
+      }
+      else {
+        return res.status(500).json({ error: 'User already exists with this email' });
+      }
     } catch (error) {
       console.error(error);
-      return res.status(500).json({ error: 'User already exists with this email' });
+      return res.status(500).json({ error: 'Error creating user' });
     }
   }
 };
