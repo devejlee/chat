a37e9e698d009667c3c49bbde81396d7a0c56957
Elijah Lee <develijahlee@gmail.com>
2023-01-31 11:05:30 +0900

    feat: add chatMessages

--- components/Message/Message.tsx
+++ components/Message/Message.tsx
@@ -1,7 +1,9 @@
 'use client';
 import styles from './Message.module.scss';
 import Image from 'next/image';
-import React, { useRef } from 'react';
+import { useSession } from 'next-auth/react';
+import React, { useRef, useContext } from 'react';
+import { ChatContext } from '@/context/ChatContext';
 
 interface MessageProps {
   message: any;
@@ -9,11 +11,14 @@ interface MessageProps {
 
 export default function Message({ message }: MessageProps) {
   const ref = useRef<HTMLDivElement>(null);
+  const { data: chatContextData } = useContext(ChatContext);
+  const { data: session } = useSession();
+
   return (
     <div ref={ref}>
       <div className={styles.messageInfo}>
         <Image
-          src={''}
+          src={session?.user?.image ? session?.user?.image : ''}
           alt="user image"
           width={24}
           height={24}
@@ -22,7 +27,7 @@ export default function Message({ message }: MessageProps) {
       </div>
       <div className={styles.messageContent}>
         <p>{message.text}</p>
-        {message.img && <Image src={''} alt="user image" width={24} height={24} />}
+        {message.image && <Image src={chatContextData.user.image} alt="user image" width={24} height={24} />}
       </div>
     </div>
   );
--- components/Messages/Messages.tsx
+++ components/Messages/Messages.tsx
@@ -1,18 +1,18 @@
 'use client';
 import styles from './Messages.module.scss';
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import Message from '../Message/Message';
 import { ChatContext } from '@/context/ChatContext';
 import { useMessages } from '@/hooks/useMessages';
 
 export default function Messages() {
-  const [messages, setMessages] = useState([]);
   const { data: chatContextData } = useContext(ChatContext);
-  const { data, isLoading, mutate } = useMessages(chatContextData.chatId ? chatContextData.chatId : '', chatContextData.chatId !== null);
+  const chatMessages = useMessages(chatContextData.chatId ? chatContextData.chatId : '', chatContextData.chatId !== null);
   return (
     <div className={styles.messages}>
-      {messages.map((m: any) => (
-        <Message message={m} key={m.id} />
+      {chatMessages.isLoading && <p>Loading...</p>}
+      {chatMessages.data && chatMessages.data.chats.messages.map((message: any) => (
+        <Message message={message} key={message.id} />
       ))}
     </div>
   );
