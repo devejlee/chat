e9400525eb127b7c188bf112ff218a22b91dcdf7
Elijah Lee <develijahlee@gmail.com>
2023-02-01 20:39:43 +0900

    modify: use 'en-US' for toLocaleTimeString

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,4 @@
 export const formatTimestamp = (_seconds: number): string => {
   const date = new Date(_seconds * 1000);
-  return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  return date.toLocaleTimeString(['en-US'], { hour: '2-digit', minute: '2-digit' });
 };
