58790939dae82219a8ed477279cf3c00a788d4cc
Elijah Lee <develijahlee@gmail.com>
2023-02-01 21:24:31 +0900

    refactor: NumpadEnter

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -26,7 +26,7 @@ export default function Search({ select }: SearchProps) {
   };
 
   const handleKey: KeyboardEventHandler<HTMLInputElement> = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' || e.key === 'NumpadEnter') {
       handleSearch();
     }
   };
